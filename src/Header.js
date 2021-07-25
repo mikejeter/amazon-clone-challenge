2221cab7c58fe6491b9b37bc1447f6e0596286c3
mikejeter <mjeter45@yahoo.com>
2021-07-25 11:54:59 -0400

    added header components

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,106 @@
 import React from 'react';
 import styled from 'styled-components';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
     return (
-        <div>
+      <Container>
          <HeaderLogo>
-          
+             <img src={"https://www.michael-jeter.com/amazon_PNG11.png"}/>
          </HeaderLogo>
-      </div>
+         <HeaderOptionAdress>
+             <OptionLineOne>Hello</OptionLineOne>
+             <OptionLineTwo>Select your address</OptionLineTwo>
+
+         </HeaderOptionAdress>
+         <HeaderSearch>
+             <SearchInput type="text" />
+             <HeaderSearchIcon>
+                <SearchIcon />
+            </HeaderSearchIcon>
+         </HeaderSearch>
+         <HeaderNavItems>
+             <HeaderOption>
+                 <OptionLineOne>Hello Mike</OptionLineOne>
+                 <OptionLineTwo>Account & List</OptionLineTwo>
+             </HeaderOption>
+             <HeaderOption>
+                 <OptionLineOne>Returns</OptionLineOne>
+                 <OptionLineTwo>& Options</OptionLineTwo>
+             </HeaderOption>
+             <HeaderOptionCart>
+                 <ShoppingBasketIcon />
+                 <CartCount>5</CartCount>
+
+             </HeaderOptionCart>
+         </HeaderNavItems>
+      </Container>
         
     )
 }
 
 export default Header
 
-const HeaderLogo = styled.div`
-height: 60px;
+const Container = styled.div`
+height: 60 px;
 background-color: #0f1111;
+color: white;
+display: flex;
+align-items: center;
+justify-content: space-between;
+`
+const HeaderLogo = styled.div`
+   img {
+       width: 100px;
+       margin-left: 11px;
+   }
+
+`
+
+const HeaderOptionAdress = styled.div`
+
+`
+
+const OptionLineOne = styled.div`
+
+`
+
+const OptionLineTwo = styled.div`
+font-weight: 700;
+`
+
+const HeaderSearch = styled.div`
+display: flex;
+flex-grow: 1;
+height: 40px;
+`
+
+const SearchInput = styled.input`
+flex-grow: 1;
+`
+
+const HeaderSearchIcon = styled.div`
+background-color: #febd69;
+width: 45px;
+color: black;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+const HeaderNavItems = styled.div`
+display: flex;
+`
+
+const HeaderOption = styled.div`
+padding: 10px 9px 10px 9px;
+`
+
+const HeaderOptionCart = styled.div`
+
+`
+
+const CartCount = styled.div`
+
 `
