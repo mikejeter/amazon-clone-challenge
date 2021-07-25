11935a7f9a81bef04c4d93128c52dfddb1e4efa4
mikejeter <mjeter45@yahoo.com>
2021-07-25 02:30:23 -0400

    Created Header.js file

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <div>
+         <HeaderLogo>
+          
+         </HeaderLogo>
+      </div>
+        
+    )
+}
+
+export default Header
+
+const HeaderLogo = styled.div`
+height: 60px;
+background-color: #0f1111;
+`
